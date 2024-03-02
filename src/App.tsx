572d8f27b489bf1b8cf0ca07cb1f1e7053a164fc
Troan89 <Wiki-Mars@yandex.ru>
2024-03-02 23:11:51 +0400

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      {/*<Button href={'https://yandex.ru'}>Hello</Button>*/}
+      <Button as={'a'} href={'https://yandex.ru'}>
+        Hello
+      </Button>
+    </div>
+  )
 }
