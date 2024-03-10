688e38fb813374b4a0a23aa58cad033a5a0a1d98
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2024-03-10 14:15:10 +0300

    Fix select component

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,4 +1,4 @@
-import React, {ComponentPropsWithoutRef, forwardRef, useState} from 'react'
+import React, { ComponentPropsWithoutRef, forwardRef, useState } from 'react'
 
 import { Typography } from '@/components/ui/typography'
 import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
@@ -8,8 +8,9 @@ import { clsx } from 'clsx'
 import s from './select.module.scss'
 
 type SelectProps = {
+  defaultValue?: string
   disabled?: boolean
-  label: string
+  label?: string
   onChange?: (value: string) => void
   placeholder?: string
   title?: string
@@ -18,62 +19,64 @@ type SelectProps = {
 
 type Props = SelectProps & ComponentPropsWithoutRef<typeof SelectFromRadix.Root>
 
+export const Select = forwardRef<HTMLButtonElement, Props>(
+  ({ children, defaultValue, disabled, onChange, placeholder, title, value }: Props, ref) => {
+    const classNames = {
+      Icon: clsx(disabled && s.disabled),
+      SelectContent: s.SelectContent,
+      SelectTrigger: clsx(s.SelectTrigger, disabled && s.SelectTriggerDisabled),
+      SelectViewport: s.SelectViewport,
+      TypographyTitle: clsx(s.Title, disabled && s.disabled),
+    }
 
+    const [isOpen, setIsOpen] = useState(false)
+    const handleOpen = () => {
+      setIsOpen(true)
+    }
+    const handleClose = () => {
+      setIsOpen(false)
+    }
 
-export const Select = ({ disabled, label, onChange, placeholder, title, value }: Props) => {
-  const classNames = {
-    Icon: clsx(disabled && s.disabled),
-    SelectContent: s.SelectContent,
-    SelectTrigger: clsx(s.SelectTrigger, disabled && s.SelectTriggerDisabled),
-    SelectViewport: s.SelectViewport,
-    TypographyTitle: clsx(s.Title, disabled && s.disabled),
+    return (
+      <SelectFromRadix.Root
+        defaultValue={defaultValue}
+        disabled={disabled}
+        onOpenChange={isOpen ? handleClose : handleOpen}
+        onValueChange={onChange}
+        value={value}
+      >
+        <Typography className={classNames.TypographyTitle} variant={'body2'}>
+          {title}
+        </Typography>
+        <SelectFromRadix.Trigger aria-label={'Food'} className={classNames.SelectTrigger} ref={ref}>
+          <SelectFromRadix.Value placeholder={placeholder} />
+          <SelectFromRadix.Icon className={classNames.Icon}>
+            {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          </SelectFromRadix.Icon>
+        </SelectFromRadix.Trigger>
+        <SelectFromRadix.Portal>
+          <SelectFromRadix.Content
+            className={classNames.SelectContent}
+            collisionPadding={0}
+            position={'popper'}
+          >
+            <SelectFromRadix.ScrollUpButton>
+              <ChevronUpIcon />
+            </SelectFromRadix.ScrollUpButton>
+            <SelectFromRadix.Viewport className={classNames.SelectViewport}>
+              {children}
+            </SelectFromRadix.Viewport>
+            <SelectFromRadix.ScrollDownButton>
+              <ChevronDownIcon />
+            </SelectFromRadix.ScrollDownButton>
+          </SelectFromRadix.Content>
+        </SelectFromRadix.Portal>
+      </SelectFromRadix.Root>
+    )
   }
+)
 
-  const [isOpen, setIsOpen] = useState(false)
-  const handleOpen = () => {
-    setIsOpen(true)
-  }
-  const handleClose = () => {
-    setIsOpen(false)
-  }
-
-  return (
-    <SelectFromRadix.Root
-      disabled={disabled}
-      onOpenChange={isOpen ? handleClose : handleOpen}
-      onValueChange={onChange}
-    >
-      <Typography className={classNames.TypographyTitle} variant={'body2'}>
-        {title}
-      </Typography>
-      <SelectFromRadix.Trigger aria-label={'Food'} className={classNames.SelectTrigger}>
-        <SelectFromRadix.Value placeholder={placeholder} />
-        <SelectFromRadix.Icon className={classNames.Icon}>
-          {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-        </SelectFromRadix.Icon>
-      </SelectFromRadix.Trigger>
-      <SelectFromRadix.Portal>
-        <SelectFromRadix.Content
-          className={classNames.SelectContent}
-          collisionPadding={0}
-          position={'popper'}
-        >
-          <SelectFromRadix.Viewport className={classNames.SelectViewport}>
-            <SelectFromRadix.Group>
-              <SelectItem value={value || ''}>{label}</SelectItem>
-              <SelectItem value={'banana'}>{label}</SelectItem>
-              <SelectItem value={'blueberry'}>{label}</SelectItem>
-              <SelectItem value={'grapes'}>{label}</SelectItem>
-              <SelectItem value={'pineapple'}>{label}</SelectItem>
-            </SelectFromRadix.Group>
-          </SelectFromRadix.Viewport>
-        </SelectFromRadix.Content>
-      </SelectFromRadix.Portal>
-    </SelectFromRadix.Root>
-  )
-}
-
-const SelectItem = forwardRef<
+export const SelectItem = forwardRef<
   HTMLDivElement,
   React.PropsWithChildren<{ className?: string; disabled?: boolean; value: string }>
 >(({ children, className, value, ...props }, forwardedRef) => {
@@ -82,7 +85,12 @@ const SelectItem = forwardRef<
   }
 
   return (
-    <SelectFromRadix.Item className={classNames.TypographyTitle} {...props} ref={forwardedRef} value={value}>
+    <SelectFromRadix.Item
+      className={classNames.TypographyTitle}
+      {...props}
+      ref={forwardedRef}
+      value={value}
+    >
       <Typography variant={'body1'}>
         <SelectFromRadix.ItemText>{children}</SelectFromRadix.ItemText>
       </Typography>
