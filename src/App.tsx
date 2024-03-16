88e64be8012cf4fc9b8591b50bc872e051328bee
Halina2610 <vievamaxa@gmail.com>
2024-03-17 02:07:02 +0300

    signIn (fix) end signUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,17 @@
-import { Button } from '@/components/ui/button'
-import { Typography } from '@/components/ui/typography'
-import { Slider } from '@/components/ui/slider'
+import { BrowserRouter } from 'react-router-dom'
+
+import { FormValues, SignIn } from '@/components/auth/signIn'
+import { SignUp } from '@/components/auth/signUp'
 
 export function App() {
+  const handlePerPageChange = (value: FormValues) => {
+    console.log(value)
+  }
+
   return (
-    <div>
-      {/*<Button href={'https://yandex.ru'}>Hello</Button>*/}
-      <Button as={'a'} href={'https://yandex.ru'}>
-        Hello Ivan commit
-      </Button>
-      <Typography as={'h1'} variant={'h1'}>
-        H1
-      </Typography>
-      <Typography as={'h2'} variant={'h2'}>
-        H2
-      </Typography>
-      <Typography as={'h3'} variant={'h3'}>
-        H3
-      </Typography>
-      <Typography as={'h4'} variant={'h4'}>
-        H4
-      </Typography>
-      <Typography as={'p'} variant={'body1'}>
-        body1
-      </Typography>
-      <Typography as={'p'} variant={'body2'}>
-        body2
-      </Typography>
-      <Typography as={'p'} variant={'caption'}>
-        caption
-      </Typography>
-      <Typography as={'p'} variant={'overline'}>
-        overline
-      </Typography>
-      <Typography as={'p'} variant={'subtitle1'}>
-        subtitle1
-      </Typography>
-      <Typography as={'p'} variant={'subtitle2'}>
-        subtitle2
-      </Typography>
-      <Typography as={'a'} variant={'link1'}>
-        link1
-      </Typography>
-      <Typography as={'a'} variant={'link2'}>
-        link2
-      </Typography>
-      <Slider max={120} min={10} step={10} />
-    </div>
+    <BrowserRouter>
+      <SignIn onSubmit={handlePerPageChange} />
+      <SignUp onSubmit={handlePerPageChange} />
+    </BrowserRouter>
   )
 }
--- src/components/auth/signUp/signUp.tsx
+++ src/components/auth/signUp/signUp.tsx
@@ -1,7 +1,6 @@
 import { useForm } from 'react-hook-form'
 import { NavLink } from 'react-router-dom'
 
-import { FormCheckbox } from '@/components/formComponents/formCheckbox'
 import { FormTextField } from '@/components/formComponents/formTextField'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
@@ -9,26 +8,28 @@ import { Typography } from '@/components/ui/typography'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
-import s from './signIn.module.scss'
+import s from './signUp.module.scss'
 
 const loginSchema = z.object({
+  confirmPassword: z.string().min(3),
   email: z.string().email(),
   password: z.string().min(3),
   rememberMe: z.boolean().default(false),
 })
 
-export type FormValues = z.infer<typeof loginSchema>
+type FormValues = z.infer<typeof loginSchema>
 
-type LoginProps = {
+type LogProps = {
   onSubmit: (data: FormValues) => void
 }
-export const SignIn = ({ onSubmit }: LoginProps) => {
+export const SignUp = ({ onSubmit }: LogProps) => {
   const {
     control,
     formState: { errors },
     handleSubmit,
   } = useForm<FormValues>({
     defaultValues: {
+      confirmPassword: '',
       email: '',
       password: '',
       rememberMe: false,
@@ -37,11 +38,11 @@ export const SignIn = ({ onSubmit }: LoginProps) => {
   })
 
   return (
-    <Card className={s.cardForm}>
+    <Card className={s.cardSignUp}>
       <Typography className={s.title} variant={'h1'}>
-        Sign In
+        Sign Up
       </Typography>
-      <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <form className={s.formRoot} onSubmit={handleSubmit(onSubmit)}>
         <div className={s.email}>
           <FormTextField
             control={control}
@@ -60,21 +61,21 @@ export const SignIn = ({ onSubmit }: LoginProps) => {
             type={'password'}
           />
         </div>
-        <FormCheckbox
-          className={s.checkbox}
-          control={control}
-          label={'remember me'}
-          name={'rememberMe'}
-        />
-        <Typography as={NavLink} className={s.recoverLink} to={'#'} variant={'body1'}>
-          Forgot Password?
-        </Typography>
+        <div className={s.confirmPassword}>
+          <FormTextField
+            control={control}
+            error={errors.password?.message}
+            label={'Confirm password'}
+            name={'confirmPassword'}
+            type={'password'}
+          />
+        </div>
         <Button as={NavLink} className={s.btn} to={'#'} type={'submit'} variant={'primary'}>
-          Sign In
+          Sing Up
         </Button>
       </form>
       <Typography className={s.typography} variant={'body2'}>
-        Don&apos;t have an account?
+        Already have an account?
       </Typography>
       <Typography
         as={NavLink}
@@ -83,7 +84,7 @@ export const SignIn = ({ onSubmit }: LoginProps) => {
         to={'#'}
         variant={'subtitle1'}
       >
-        Sign Up
+        Sing In
       </Typography>
     </Card>
   )
