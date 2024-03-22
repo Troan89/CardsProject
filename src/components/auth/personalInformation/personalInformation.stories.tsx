d98442dc49f29cd7b0af671fcc337968fc141075
Halina2610 <vievamaxa@gmail.com>
2024-03-23 01:58:48 +0300

    fix PersonalInformation

--- src/components/auth/personalInformation/personalInformation.stories.tsx
+++ src/components/auth/personalInformation/personalInformation.stories.tsx
@@ -2,21 +2,34 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { PersonalInformation } from '@/components/auth/personalInformation/personalInformation'
 
-const meta = {
+import avatar from '../../../assets/img/avatarDemo.jpeg'
+
+const meta: Meta<typeof PersonalInformation> = {
   component: PersonalInformation,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
   title: 'Auth/PersonalInformation',
-} satisfies Meta<typeof PersonalInformation>
+}
 
 export default meta
+
 type Story = StoryObj<typeof meta>
 
+// @ts-ignore
 export const DefaultEditProfile: Story = {
   args: {
-    email: '123@yandex.ru',
-    name: 'Ivan',
+    // @ts-ignore
+    data: {
+      avatar: avatar,
+      email: 'example@gmail.com',
+      name: 'User',
+    },
+    logout: () => {},
+    updateAvatar: async (avatar: File) => {
+      console.log('Updating avatar:', avatar)
+    },
+    updateName: () => {},
   },
 }
