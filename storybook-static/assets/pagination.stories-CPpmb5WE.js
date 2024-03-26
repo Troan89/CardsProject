5ff7da9bb8fe4eafeafe352d062001f1636e2dcb
Halina2610 <vievamaxa@gmail.com>
2024-03-26 12:12:30 +0300

    add toast, backButton.tsx and fix

---
+++ storybook-static/assets/pagination.stories-CPpmb5WE.js
@@ -0,0 +1,9 @@
+import{P as n}from"./pagination-DLhb3JAZ.js";import"./jsx-runtime-DzgN-JE8.js";import"./index-CO9pbFv1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Icons-i9HqDeSz.js";import"./select-DERBuwbl.js";import"./typography-BUUkRT78.js";import"./clsx-B-dksMZM.js";import"./extends-CCbyfPlC.js";import"./index-BT32HCm8.js";import"./index-BEk9yI-S.js";import"./index-Dwm3ODnT.js";import"./index-DtwMkvya.js";import"./Combination-BY04mCsb.js";import"./index-gwQh6S1x.js";import"./index-DiDi1Ixm.js";import"./index-CjLZEfCi.js";import"./index-iWjfmL1M.js";const O={component:n,tags:["autodocs"],title:"Components/Pagination"},o={args:{count:20,limit:10,onChange:()=>{},page:10,siblings:1}};var t,r,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    count: 20,
+    limit: 10,
+    onChange: () => {},
+    page: 10,
+    siblings: 1
+  }
+}`,...(i=(r=o.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const S=["Controlled"];export{o as Controlled,S as __namedExportsOrder,O as default};
---
+++ storybook-static/assets/textField.stories-BKYhoZeI.js
@@ -0,0 +1,37 @@
+import{T as w}from"./textField-DaZ4w6xp.js";import"./jsx-runtime-DzgN-JE8.js";import"./index-CO9pbFv1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Icons-i9HqDeSz.js";import"./typography-BUUkRT78.js";import"./clsx-B-dksMZM.js";const R={component:w,tags:["autodocs"],title:"Components/TextField"},e={args:{className:"error",error:"Error message",label:"Input with error",onChange:()=>{console.log("ERROR")},type:"text",value:"Error"}},r={args:{label:"Input with email",onChange:()=>{console.log("email")},type:"email"}},a={args:{label:"Input with password",onChange:()=>{console.log("password")},type:"password"}},o={args:{label:"Input with search",onChange:()=>{console.log("Search")},type:"search",value:"Input search"}};var s,n,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    className: 'error',
+    error: 'Error message',
+    label: 'Input with error',
+    onChange: () => {
+      console.log('ERROR');
+    },
+    type: 'text',
+    value: 'Error'
+  }
+}`,...(t=(n=e.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};var l,c,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Input with email',
+    onChange: () => {
+      console.log('email');
+    },
+    type: 'email'
+  }
+}`,...(p=(c=r.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var m,i,g;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Input with password',
+    onChange: () => {
+      console.log('password');
+    },
+    type: 'password'
+  }
+}`,...(g=(i=a.parameters)==null?void 0:i.docs)==null?void 0:g.source}}};var u,d,h;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'Input with search',
+    onChange: () => {
+      console.log('Search');
+    },
+    type: 'search',
+    value: 'Input search'
+  }
+}`,...(h=(d=o.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};const v=["Error","Email","Password","Search"];export{r as Email,e as Error,a as Password,o as Search,v as __namedExportsOrder,R as default};
