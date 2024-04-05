4367831284010eb1feced966d3a90afb79a5eb7a
Troan89 <Wiki-Mars@yandex.ru>
2024-04-05 10:21:54 +0400

    fix

--- src/components/learnCards/learnCards.tsx
+++ src/components/learnCards/learnCards.tsx
@@ -24,7 +24,6 @@ const radio: Options[] = [
 
 export const LearnCards = () => {
   const [open, setOpen] = useState<boolean>(false)
-  const [countAnswer, setCountAnswer] = useState<number>(10)
   const [nextQuestion, setNextQuestion] = useState<string>()
   const [grade, setGrade] = useState<number>(0)
 
@@ -58,7 +57,7 @@ export const LearnCards = () => {
           <Typography variant={'body1'}>Question: {data?.question}</Typography>
           {data?.questionImg && <img alt={'question Image'} src={data?.questionImg} />}
           <Typography className={s.countAnswer} variant={'body2'}>
-            Количество попыток ответов на вопрос: {countAnswer}
+            Количество попыток ответов на вопрос: {data?.shots}
           </Typography>
           {!open && (
             <Button fullWidth onClick={() => setOpen(true)} variant={'primary'}>
